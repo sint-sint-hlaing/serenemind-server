ef82b1e1815c693dc544b4daeb6506a19272a1cc
Sint Sint Hlaing <sints2001@gmail.com>
2026-08-05 14:17:12 +0630

    all work

--- src/main/java/com/mental/repository/ChatMessageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mental.repository;
-
-import com.mental.model.entity.ChatMessage;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
-    List<ChatMessage> findBySessionIdOrderByCreatedAtAsc(Long sessionId);
-}
--- src/main/java/com/mental/repository/ChatSessionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mental.repository;
-
-import com.mental.model.entity.ChatSession;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface ChatSessionRepository extends JpaRepository<ChatSession, Long> {
-    List<ChatSession> findByUserIdOrderByUpdatedAtDesc(Long userId);
-}
